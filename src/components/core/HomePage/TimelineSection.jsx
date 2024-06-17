9da19f8a2ecc5375413d8ea91a3632bf4383bceb
Demo-repo <ps469004@gmail.com>
2024-06-17 23:35:53 +0530

    initial commit

---
+++ src/components/core/HomePage/TimelineSection.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const timeline=[
+    {
+        Logo:Logo1,
+        heading:"Leadership",
+        Description:"Fully committed to the success company"
+    },
+        {
+        Logo:Logo2,
+        heading:"Leadership",
+        Description:"Fully committed to the success company"
+    },
+        {
+        Logo:Logo3,
+        heading:"Leadership",
+        Description:"Fully committed to the success company"
+    },
+        {
+        Logo:Logo4,
+        heading:"Leadership",
+        Description:"Fully committed to the success company"
+    }
+]
+const TimelineSection = () => {
+  return (
+    <div>
+      <div className='flex flex-row gap-15 items-center'>
+        <div className='w-[45%] flex flex-col gap-5 '></div>
+      </div>
+    </div>
+  )
+}
+
+export default TimelineSection
