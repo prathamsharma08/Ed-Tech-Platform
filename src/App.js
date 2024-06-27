e8d0bf2654819b6ed3d477422d0ba11e94b3f809
Demo-repo <ps469004@gmail.com>
2024-06-27 23:57:09 +0530

    Added Forgot password and more components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,39 @@ import { Route,Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/common/Navbar";
 import Home from "./pages/Home";
+import ForgotPassword from "./pages/ForgotPassword";
 function App() {
   return (
     <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
      <Navbar/>
       <Routes>
         <Route path="/" element={<Home/>}></Route>
+      {/* <Route
+          path="signup"
+          element={
+            <OpenRoute>
+              <Signup />
+            </OpenRoute>
+          }
+          /> */}
+{/*         
+    <Route
+          path="login"
+          element={
+            <OpenRoute>
+              <Login />
+            </OpenRoute>
+          }
+        /> */}
+
+    <Route
+          path="forgot-password"
+          element={
+            <OpenRoute>
+              <ForgotPassword />
+            </OpenRoute>
+          }
+        />  
       </Routes>
     </div>
   )
