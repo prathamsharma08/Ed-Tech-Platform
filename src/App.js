d59dc4be20216a0a360236205e3e7d8ea7a09973
Demo-repo <ps469004@gmail.com>
2024-06-28 23:58:04 +0530

    Created Login pages

--- src/App.js
+++ src/App.js
@@ -2,22 +2,25 @@ import { Route,Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/common/Navbar";
 import Home from "./pages/Home";
+import OpenRoute from "./components/core/Auth/OpenRoute";
 import ForgotPassword from "./pages/ForgotPassword";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 function App() {
   return (
     <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
      <Navbar/>
       <Routes>
         <Route path="/" element={<Home/>}></Route>
-      {/* <Route
+      <Route
           path="signup"
           element={
             <OpenRoute>
               <Signup />
             </OpenRoute>
           }
-          /> */}
-{/*         
+          /> 
+       
     <Route
           path="login"
           element={
@@ -25,7 +28,7 @@ function App() {
               <Login />
             </OpenRoute>
           }
-        /> */}
+        />
 
     <Route
           path="forgot-password"
